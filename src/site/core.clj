1e7d53178d1f94c22da5d1be76ebb7802a5634ea
Paulus Esterhazy <pesterhazy@gmail.com>
2016-07-31 19:49:18 +0200

    Add highlight.js for code block

--- src/site/core.clj
+++ src/site/core.clj
@@ -1,12 +1,18 @@
 (ns site.core
   (:require [hiccup.page :as hp]))
 
+
 (defn page [data]
   (hp/html5
    [:head
+    (hp/include-css "/css/style.css")
     (hp/include-css "/vendor/basscss@8.0.1.min.css")
-    (hp/include-css "/css/style.css")]
+    (hp/include-css "/vendor/highlight.css")
+
+    (hp/include-js "/vendor/highlight.js")]
    [:body
     [:div.max-width-3.mx-auto
      [:div.clearfix
-      (-> data :entry :content)]]]))
+      (-> data :entry :content)]]
+
+    [:script "hljs.initHighlightingOnLoad();"]]))
