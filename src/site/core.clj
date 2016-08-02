0a38f1ed7f018564fe5d8562b6bf6073db7a0c84
Paulus Esterhazy <pesterhazy@gmail.com>
2016-08-02 23:45:34 +0200

    Add GA

--- src/site/core.clj
+++ src/site/core.clj
@@ -13,7 +13,14 @@
     (hp/include-css "/vendor/highlight.css")
 
     (hp/include-js "/vendor/highlight.js")
-    [:script "hljs.initHighlightingOnLoad();"]]
+    [:script "hljs.initHighlightingOnLoad();"]
+    [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-81846608-1', 'auto');
+  ga('send', 'pageview');"]]
    [:body
     [:div.max-width-3.mx-auto
      [:div.clearfix
