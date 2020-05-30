9be0df81a31ada70bac5b2d24eeb7480eeae6b4e
Paulus Esterhazy <pesterhazy@gmail.com>
2020-05-30 16:04:42 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-function run() {
-  console.log("ii");
+const util = require("util");
+const execFile = util.promisify(require("child_process").execFile);
+
+async function run() {
+  let { stdout } = await execFile("node", ["--version"]);
+
+  console.log(stdout);
 }
 
 run();
