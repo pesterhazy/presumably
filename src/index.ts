1007a322f3075705107f839d0c6d3662cba71a9c
Paulus Esterhazy <pesterhazy@gmail.com>
2020-05-30 19:49:54 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 
-async function run() {
+async function transform() {
   await execFile("pandoc", [
     "--output=out/index.html",
     "--to=html5",
     "--data-dir",
     ".",
     "--template",
-    "presumably.html5",
+    "presumably.html5", // FIXME
     "posts/monorepos.md"
   ]);
   console.log("ok");
+}
+
+async function run() {
+  try {
+    await transform();
+  } catch (e) {
+    console.error("Failed\n" + e.stack);
+    process.exit(1);
+  }
 }
 
 run();
